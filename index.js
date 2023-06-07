7cbeaf927024e200a8581d083846949b0689f970
saddansyah <saddanakbar@gmail.com>
2023-06-07 08:59:39 +0700

    chore: vercel settings

--- index.js
+++ index.js
@@ -25,11 +25,19 @@ app.use(morgan('dev'));
 connectDB();
 
 // express third party middlewares
-app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 // Use route
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/api/contacts', contactsRoute);
 app.use('/api/articles', articlesRoute);
 // app.use('/api/tasks', taskRouter);
